2d50028850054b9d8a092a1f2a5b71942e1f3fb8
Joaovsam <joaow1234@gmail.com>
2024-10-09 16:25:48 -0300

    Uml Atividade Iphone

---
+++ UmlIphone/src/model/Iphone.java
@@ -0,0 +1,50 @@
+package model;
+
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Pausando música");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música " + musica + " selecionada para tocar.");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Pagina " + url + " iniciada");
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Aba Adicionada");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("pagina atualizada");
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para o número: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("");
+    }
+
+}
